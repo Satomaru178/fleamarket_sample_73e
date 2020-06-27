b84a763a1d82fe4f318f872f4872e6b794969a6e
Yuta1634 <yuta1994year@gmail.com>
2020-06-27 20:45:26 +0900

    Create User_login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :email, :password, :password_confirmation, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+  validates :birthday, presence: true
+  validates :nickname, :email, uniqueness: true, presence: true
   validates :first_name, :last_name, presence: true,
                  format: {
                    with:/\A[ぁ-んァ-ン一-龥]/,
