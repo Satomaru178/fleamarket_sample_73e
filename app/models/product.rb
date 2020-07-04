cb22542c6a019fc11c6f3408e7c2c900f3206f3c
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-05 00:55:15 +0900

    edit models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  has_many :seller, class_name: 'User', through: :user_products
+  has_many :buyer,  class_name: 'User', through: :user_products
+  has_many :images
+  has_many :user_products
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :costburden
+  belongs_to_active_hash :shippingorigin
+  belongs_to_active_hash :shippingperiod
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :birthday, presence: true
+
   validates :nickname, uniqueness:true, presence: true
+
   validates :email, uniqueness: true
+
   validates :first_name, :last_name,
-                 format: {
-                   with:/\A[ぁ-んァ-ン一-龥]/,
-                 }
+  format: {
+     with:/\A[ぁ-んァ-ン一-龥]/,
+  }
+
   validates :password,
   format: {
     with: /\A[a-zA-Z0-9]+\z/
   }
+
   validates :first_name_kana, :last_name_kana,
   format: {
-  with:/\A[ぁ-んー－]+\z/,
+    with:/\A[ぁ-んー－]+\z/,
   }
-  has_one :address
+
+  has_one  :address
+  has_many :products, through: :user_products
+  has_many :user_products
 end
