1872c50223d89607788941980d7ba3f55cdb5dd5
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-05 00:55:37 +0900

    edit migration files

--- db/migrate/20200704132156_create_products.rb
+++ db/migrate/20200704132156_create_products.rb
@@ -1,7 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.string  :name
+      t.text    :explain
+      t.integer :condition_id
+      t.integer :costburden_id
+      t.integer :shippingorigin_id
+      t.integer :shippingperiod_id
+      t.integer :price
       t.timestamps
     end
   end
