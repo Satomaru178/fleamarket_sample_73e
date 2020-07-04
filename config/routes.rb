9ecbd828b43b7fb22180238b5bd73b696f9aa4ee
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-04 21:55:48 +0900

    edit route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'top#index'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+
   devise_scope :user do
-    get 'addresses', to: 'users/registrations#new_address'
+    get  'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
+  resources :products, except: :show
 end
