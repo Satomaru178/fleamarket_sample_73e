7c65aedacfb4c01bb4bbfe5e901cca4e6ddb4677
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-05 02:20:57 +0900

    edit product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,16 @@
 class ProductsController < ApplicationController
-  def index
-  end
-
   def new
+    @product = Product.new
+    @product.images.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +20,20 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :explain,
+      :condition_id,
+      :costburden_id,
+      :shippingorigin_id,
+      :shippingperiod_id,
+      :price,
+      images_attributes: [:src]
+    )
   end
 end
