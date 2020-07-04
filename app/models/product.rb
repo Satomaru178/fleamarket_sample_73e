1d7f1bebadc8a79c5ecf4123cf25720bcc2f76fe
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-05 04:35:40 +0900

    swap a description

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,10 @@ class Product < ApplicationRecord
   has_many :buyer,  class_name: 'User', through: :user_products
   has_many :images, dependent: :destroy
   has_many :user_products
+  accepts_nested_attributes_for :images, allow_destroy: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
   belongs_to_active_hash :costburden
   belongs_to_active_hash :shippingorigin
   belongs_to_active_hash :shippingperiod
-  accepts_nested_attributes_for :images, allow_destroy: true
 end
