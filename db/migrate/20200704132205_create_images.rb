b61782cdff97e288de6601d89a4e4463bb10780d
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-05 01:26:35 +0900

    rename column of images table

--- db/migrate/20200704132205_create_images.rb
+++ db/migrate/20200704132205_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.references :product, foreign_key: true
-      t.string     :image
+      t.string     :src
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_07_04_152452) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "product_id"
-    t.string "image"
+    t.string "src"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_images_on_product_id"
