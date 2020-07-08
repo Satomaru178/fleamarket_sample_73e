5b7d2f85f830527b33559e51ebf934d124a79742
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-08 22:25:56 +0900

    header-menu_create

---
+++ app/assets/javascripts/header-menu.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(".header__contents__lower__left-items__categories").hover(function(){
+    $(".category1").addClass("appeared");
+  });
+  $(".parent__list").hover(function(){
+    $(".category2").addClass("appeared");
+  });
+  $(".child__list").hover(function(){
+    $(".category3").addClass("appeared");
+  });
+});
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    @parents = Category.all.order("id ASC").limit(13)
   end
 end
