176015f8c30244a1275e2c6082ff2b49653e2941
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-08 16:13:55 +0900

    enter space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
