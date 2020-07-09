74f8461e336e90dd6867fa9df02a27f9932c647e
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-09 19:46:13 +0900

    sorting lines

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,21 @@
 class Product < ApplicationRecord
-  has_many :user_products, dependent: :destroy
-  has_many :seller, class_name: 'User', through: :user_products
-  has_many :buyer,  class_name: 'User', through: :user_products
+  # has_many :user_products, dependent: :destroy
+  # has_many :seller, class_name: 'User', through: :user_products
+  # has_many :buyer,  class_name: 'User', through: :user_products
+
+  # images
+
   has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images, allow_destroy: true
+
+  # activehash
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
   belongs_to_active_hash :costburden
   belongs_to_active_hash :shippingorigin
   belongs_to_active_hash :shippingperiod
+
+  # validation
 
   validates :name, presence: true, length: { maximum: 40 }
 
@@ -29,6 +36,7 @@ class Product < ApplicationRecord
   validates :price, presence: true,
   numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000 }
 
+  accepts_nested_attributes_for :images, allow_destroy: true
   validates_associated :images
   validates :images, presence: true
 end
