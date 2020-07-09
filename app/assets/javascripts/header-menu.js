ae4d09a65b33b4b37c42e6bd9db91b5e88ff6b9c
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-09 23:58:02 +0900

    jquery create

--- app/assets/javascripts/header-menu.js
+++ app/assets/javascripts/header-menu.js
@@ -1,11 +1,18 @@
 $(function(){
   $(".header__contents__lower__left-items__categories").hover(function(){
     $(".category1").addClass("appeared");
+  }, function(){
+    $(".category1").removeClass("appeared");
   });
   $(".parent__list").hover(function(){
     $(".category2").addClass("appeared");
+  }, function(){
+    $(".category2").removeClass("appeared");
   });
   $(".child__list").hover(function(){
     $(".category3").addClass("appeared");
+  }, function(){
+    $(".category3").removeClass("appeared");
   });
+
 });
