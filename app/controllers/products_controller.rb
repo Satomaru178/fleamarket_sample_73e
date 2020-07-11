a0d5b73382f7859de212915ad0462da22a5ee480
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-11 12:42:34 +0900

    create module for array of parent category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,9 @@ class ProductsController < ApplicationController
   # 出品したユーザしかできない
   before_action :ensure_currect_user, only: [:edit, :update, :destroy]
 
+  # 親カテゴリの配列を用意する
+  before_action :set_categories, only: [:new, :create, :edit, :update]
+
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
@@ -12,17 +15,11 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.images.new
-    
-    # セレクトボックスの初期値設定
-    @category_parent_array = []
-    # 親カテゴリー名を抽出し配列化
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-    end
   end
 
   def create
     @product = Product.new(product_params)
+    binding.pry
     if @product.save
       redirect_to root_path and return
     else
@@ -67,20 +64,31 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(
+      :seller_id,
       :name,
       :explain,
+      :category_id,
       :condition_id,
       :costburden_id,
       :shippingorigin_id,
       :shippingperiod_id,
       :price,
-      :seller_id,
-      images_attributes: [:src, :_destroy, :id]
+      images_attributes: [:id, :src, :_destroy],
+      category_attributes: [:id],
     )
   end
 
   def category_params
-    params.require(:category).permit(:name, :ancestry)
+    params.require(:category).permit(:id, :name, :ancestry)
+  end
+
+  def set_categories
+    # セレクトボックスの初期値設定
+    @category_parent_array = []
+    # 親カテゴリー名を抽出し配列化
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
 
   def ensure_currect_user
