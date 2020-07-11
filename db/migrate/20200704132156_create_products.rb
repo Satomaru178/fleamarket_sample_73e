7be6d027fee6accfc3cb9c33a5794903d3714f44
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-11 12:43:31 +0900

    edit product table

--- db/migrate/20200704132156_create_products.rb
+++ db/migrate/20200704132156_create_products.rb
@@ -10,7 +10,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer    :price
       t.references :seller, null: false, foreign_key: { to_table: :users }
       t.references :buyer,               foreign_key: { to_table: :users }
-      t.references :category, null: false, foreign_key: true
+      t.references :category,            foreign_key: true
       t.timestamps
     end
   end
