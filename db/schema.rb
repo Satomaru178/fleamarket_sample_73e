3ef8d9b35130bb283f2c0f410de070ed06f6b04a
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-11 20:36:58 +0900

    create brand model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_042143) do
+ActiveRecord::Schema.define(version: 2020_07_11_111631) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_07_05_042143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "CreateBrands", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["CreateBrands"], name: "index_brands_on_CreateBrands"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
