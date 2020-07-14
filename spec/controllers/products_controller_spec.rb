5a5c28e6ae41820136ed8750282ae7e8935dcefa
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-14 12:51:59 +0900

    edit products controller spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -70,37 +70,41 @@ describe ProductsController do
   end
 
   describe 'GET #edit' do
+    let(:redshirt) { create :redshirt }
+
     it 'リクエストが成功すること' do
       pending 'うまくいかないのであとで修正する'
-      get :edit
+      get :edit, params: { id: redshirt }
       expect(response.status).to eq 200
     end
 
     it 'editテンプレートで表示されること' do
       pending 'うまくいかないのであとで修正する'
-      get :edit
+      get :edit, params: { id: redshirt }
       expect(response).to render_template :edit
     end
   end
 
   describe 'PUT #update' do
+    let(:redshirt) { create :redshirt }
+
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do
         pending 'うまくいかないのであとで修正する'
-        put :update, params: { id: :takashi, product: build(:satoshi) }
+        put :update, params: { id: :redshirt, product: attributes_for(:blueshirt) }
         expect(response.status).to eq 302
       end
 
-      it '商品名が更新されること' do
+      it '商品が更新されること' do
         pending 'うまくいかないのであとで修正する'
         expect do
-          put :update, params: { id: :takashi, product: build(:satoshi) }
-        end.to change { Product.find(:takashi.id).name }.from('Takashi').to('Satoshi')
+          put :update, params: { id: :redshirt, product: attributes_for(:blueshirt) }
+        end.to change { Product.find(:redshirt.id) }
       end
 
       it 'トップページにリダイレクトすること' do
         pending 'うまくいかないのであとで修正する'
-        put :update, params: { id: :takashi, product: build(:satoshi) }
+        put :update, params: { id: :redshirt, product: attributes_for(:blueshirt) }
         expect(response).to redirect_to('/')
       end
     end
@@ -108,47 +112,50 @@ describe ProductsController do
     context 'パラメータが不正な場合' do
       it 'リクエストが成功すること' do
         pending 'うまくいかないのであとで修正する'
-        put :update, params: { id: :takashi, user: build(:product, name: nil) }
+        put :update, params: { id: :redshirt, product: attributes_for(:blueshirt, name: nil) }
         expect(response.status).to eq 200
       end
 
-      it '商品名が変更されないこと' do
+      it '商品が変更されないこと' do
         pending 'うまくいかないのであとで修正する'
         expect do
-          put :update, params: { id: :takashi, user: build(:product, name: nil) }
-        end.to_not change(Product.find(takashi.id), :name)
+          put :update, params: { id: :redshirt, product: attributes_for(:blueshirt, name: nil) }
+        end.to_not change(Product.find(:redshirt.id), :name)
       end
 
       it 'editテンプレートで表示されること' do
         pending 'うまくいかないのであとで修正する'
-        put :update, params: { id: :takashi, user: build(:product, name: nil) }
+        put :update, params: { id: :redshirt, product: attributes_for(:blueshirt, name: nil) }
         expect(response).to render_template :edit
       end
 
       it 'エラーが表示されること' do
         pending 'うまくいかないのであとで修正する'
-        put :update, params: { id: :takashi, user: build(:product, name: nil) }
+        put :update, params: { id: :redshirt, product: attributes_for(:blueshirt, name: nil) }
         expect(assigns(:product).errors.any?).to be_truthy
       end
     end
   end
 
   describe 'DELETE #destroy' do
+    let(:redshirt) { create :redshirt }
+
     it 'リクエストが成功すること' do
-      delete :destroy, params: { id: :product }
+      pending 'うまくいかないのであとで修正する'
+      delete :destroy, params: { id: redshirt }
       expect(response.status).to eq 302
     end
 
     it '商品が削除されること' do
       pending 'うまくいかないのであとで修正する'
       expect do
-        delete :destroy, params: { id: :product }
+        delete :destroy, params: { id: redshirt }
       end.to change(Product, :count).by(-1)
     end
 
     it 'トップページにリダイレクトすること' do
       pending 'うまくいかないのであとで修正する'
-      delete :destroy, params: { id: :product }
+      delete :destroy, params: { id: redshirt }
       expect(response).to redirect_to('/')
     end
   end
