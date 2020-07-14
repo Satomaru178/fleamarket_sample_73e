5db195b00bafbebb52d7077561328c593c7217fe
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-07-14 11:53:12 +0900

    add info to factories

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,14 +2,42 @@ FactoryBot.define do
   factory :product do
     name               { "test" }
     explain            { "test" }
-    seller_id          { 1 }
-    buyer_id           { 2 }
-    category_id        { 1 }
-    brand_id           { 1 }
     condition_id       { 1 }
     costburden_id      { 1 }
     shippingorigin_id  { 1 }
     shippingperiod_id  { 1 }
     price              { 500 }
+    seller             { build(:takashi) }
+    buyer              { build(:satoshi) }
+    category
+    brand
+  end
+
+  factory :redshirt, class: Product do
+    name               { "redshirt" }
+    explain            { "test" }
+    condition_id       { 2 }
+    costburden_id      { 2 }
+    shippingorigin_id  { 2 }
+    shippingperiod_id  { 2 }
+    price              { 2500 }
+    seller             { build(:takashi) }
+    buyer              { build(:satoshi) }
+    category
+    brand
+  end
+
+  factory :blueshirt, class: Product do
+    name               { "blueshirt" }
+    explain            { "test" }
+    condition_id       { 3 }
+    costburden_id      { 3 }
+    shippingorigin_id  { 3 }
+    shippingperiod_id  { 3 }
+    price              { 1500 }
+    seller             { build(:satoshi) }
+    buyer              { build(:takashi) }
+    category
+    brand
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
-  
   factory :user do
     nickname              { "taro" }
     email                 { "taro@gmail.com" }
@@ -10,5 +9,29 @@ FactoryBot.define do
     first_name_kana       { "さとう" }
     last_name_kana        { "たろう" }
     birthday              { "1990-1-1" }
+  end
+
+  factory :takashi, class: User do
+    nickname              { "takashi" }
+    email                 { "takashi@example.com" }
+    password              { "password" }
+    password_confirmation { "password" }
+    first_name            { "新井" }
+    last_name             { "隆" }
+    first_name_kana       { "あらい" }
+    last_name_kana        { "たかし" }
+    birthday              { "1995-6-15" }
+  end
+
+  factory :satoshi, class: User do
+    nickname              { "satoshi" }
+    email                 { "satoshi@example.com" }
+    password              { "password" }
+    password_confirmation { "password" }
+    first_name            { "石田" }
+    last_name             { "智" }
+    first_name_kana       { "いしだ" }
+    last_name_kana        { "さとし" }
+    birthday              { "1998-8-24" }
   end
 end
