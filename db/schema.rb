3547ef0745fb375eb102e9ba9c9e4558037be505
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2020-07-18 19:01:28 +0900

    edit migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_111631) do
+ActiveRecord::Schema.define(version: 2020_07_13_132205) do
 
   create_table "accounts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
