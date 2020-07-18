b438a672c5615ff3c9afbba7a9a677c4caaa60e3
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-18 20:40:45 +0900

    modificate top_controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
     @parents = Category.where(ancestry: nil)
+    @products = Product.includes(:images).where(buyer_id: null).order(:id :desc)
   end
 end
