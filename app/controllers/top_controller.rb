31b29a1d65b9687e1132ec84ac30fc315a282ec0
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-19 17:57:23 +0900

    Modificate view

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
   def index
     @parents = Category.where(ancestry: nil)
-    @products = Product.includes(:images).where(buyer_id: null).order(:id :desc)
+    @products = Product.includes(:images).where(buyer_id: nil).order("id DESC")
+    @brandProducts = Product.includes(:images).where(buyer_id: nil).where(brand_id: 1).order("id DESC")
   end
 end
