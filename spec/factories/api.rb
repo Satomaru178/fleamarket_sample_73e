8d048f51ca94aac2299a91b5141d9447d6af750d
Yuta1634 <yuta1994year@gmail.com>
2020-07-22 20:35:57 +0900

    fixAPI

---
+++ spec/factories/api.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  
+  factory :sns_credentials do
+    provider {"google_oauth2"}
+    uid      {"107424599353959325721"}
+  end
+end
--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe SnsCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SnsCredential do
+  describe '#create' do
+
+    #1
+    it "入力された情報が正しければ登録できること" do
+      sns_credenmtial = build(:sns_credenmtial)
+      expect(sns_credenmtial).to be_valid
+    end
+  end
 end
