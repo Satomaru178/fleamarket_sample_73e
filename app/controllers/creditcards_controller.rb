fb5d9b2873a90824099d241c235658321478340a
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2020-07-25 00:01:09 +0900

    edit creditcards_controller.rb

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -20,7 +20,7 @@ class CreditcardsController < ApplicationController
   end
 
   def create
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
     if params["payjp_token"].blank?
       redirect_to action: "index"
     else
@@ -43,7 +43,7 @@ class CreditcardsController < ApplicationController
     if @card.blank?
       render action: :index
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @customer_card = customer.cards.retrieve(@card.card_id)
       @exp_month = @customer_card.exp_month.to_s
@@ -71,7 +71,7 @@ class CreditcardsController < ApplicationController
     if @card.blank?
       redirect_to action: "index"
     else
-      Payjp.api_key =  ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
       customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
       @card.delete
