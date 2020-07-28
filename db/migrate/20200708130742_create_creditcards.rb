e5d072e20abca741d3cbe443abc0545441c62d02
Yuta1634 <yuta1994year@gmail.com>
2020-07-28 15:43:50 +0900

    fix_database

--- db/migrate/20200708130742_create_creditcards.rb
+++ db/migrate/20200708130742_create_creditcards.rb
@@ -1,7 +1,7 @@
 class CreateCreditcards < ActiveRecord::Migration[5.0]
   def change
     create_table :creditcards do |t|
-      t.references :user,          null: false, forign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :customer_id, null: false
       t.string :card_id, null: false
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -109,7 +109,9 @@ ActiveRecord::Schema.define(version: 2020_07_13_132205) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "addresses", "users"
+  add_foreign_key "creditcards", "users"
   add_foreign_key "images", "products"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
