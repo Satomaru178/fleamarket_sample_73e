d8bbcbfa9e749614d22e126d658f92e192b77572
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-08-02 15:08:54 +0900

    Modificate routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'likes/destroy'
   get 'categories/index'
   root to: 'top#index'
 
@@ -17,6 +19,7 @@ Rails.application.routes.draw do
       get 'get_category_children',      defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
+    resources :likes, only: [:create, :destroy]
   end
 
   resources :brands, only: [:new, :create]
