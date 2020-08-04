055023b22b04b154b46d41baa1a04474a2e29925
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-08-04 11:21:57 +0900

    Refactoring  unnecessary-variable

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,13 @@ class LikesController < ApplicationController
   before_action :set_product
 
   def create
-    @like = Like.create(user_id: current_user.id, product_id: params[:product_id])
-    @likes = Like.where(product_id: params[:product_id])
+    @like = Like.create(user_id: current_user.id, product_id: @product.id)
     @product.reload
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, product_id: params[:product_id])
-    like.destroy
-    @likes = Like.where(product_id: params[:product_id])
+    @like = Like.find_by(user_id: current_user.id, product_id: @product.id)
+    @like.destroy
     @product.reload
   end
 
