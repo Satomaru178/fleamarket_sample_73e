58313a9e4811ce4b8d8bcb756c8074dd700f237a
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-08-04 16:22:16 +0900

    Add action_likes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,17 @@
 class AccountsController < ApplicationController
   before_action :move_to_index,  only: [:index, :logout]
-  before_action :set_categories, only: [:index, :logout]
+  before_action :set_categories, only: [:index, :logout, :likes]
 
   def index
   end
 
   def logout
+  end
+
+  def show
+  end
+
+  def likes
   end
 
   private
