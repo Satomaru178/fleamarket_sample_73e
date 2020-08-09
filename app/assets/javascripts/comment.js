a46a1be4260f1d66031055266b2d57a916e853ca
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2020-08-09 14:28:55 +0900

    Update comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,5 +1,7 @@
 $(document).on('turbolinks:load', function() {
-  // 新規コメント
+  // ===================================
+  // 新規コメント表示用・自分のコメント復元用 
+  // ===================================
   function new_comment(comment_data){
     var HTML_user =
       `
@@ -50,10 +52,11 @@ $(document).on('turbolinks:load', function() {
         </div>
       </li>
       `
+    
     if (comment_data.product_seller.id == comment_data.user_id){
       // 出品者とコメントしたユーザーが等しい場合
       var html = HTML_user + HTML_seller + HTML_body + HTML_deleteBtn + HTML_closeTag
-    }　else　{
+    } else {
       // 出品者とコメントしたユーザーが異なる場合
       var html = HTML_user + HTML_body + HTML_closeTag
     };
@@ -61,7 +64,102 @@ $(document).on('turbolinks:load', function() {
     return html; 
   };
 
-  
+  // ===================================
+  // 他人のコメント復元用 
+  // ===================================
+  function restore_other_comment(comment_data){
+    var HTML_user =
+      `
+      <li class="content-main__comments__list__block" data-index=${comment_data.id}>
+        <a class="comments__user" href="#">
+          <figure>
+            <div class="comments__user__image">
+              <img src="/assets/material/icon/member_photo_noimage_thumb.png">
+            </div>
+            <figcaption class="comments__user__name">
+              ${comment_data.user_nickname}
+            </figcaption>
+          </figure>
+      `
+    var HTML_seller =
+          `
+          <div class="comments__user__seller">
+            出品者
+          </div>
+          `
+    var HTML_body =
+      `
+        </a>
+        <div class="content-main__comments__list__block__content">
+          <div class="content-main__comments__list__block__content__text">${comment_data.comment}
+          </div>
+          <div class="content-main__comments__list__block__content__icons">
+            <time class="content-main__comments__list__block__content__icons__left">
+              <i class="fas content-main__comments__list__block__content__icons__left__time fa-clock"></i>
+              <span>
+                ${comment_data.created_at}
+              </span>
+            </time>
+            <div class="content-main__comments__list__block__content__icons__right">
+              <div class="content-main__comments__list__block__content__icons__right__flag">
+                <a href="#">
+                  <i class="far fa-flag"></i>
+                </a>
+              </div>
+              <div class="content-main__comments__list__block__content__icons__right__delete other_pre_delete" data-index=${comment_data.id}>
+                <a rel="nofollow" data-method="patch" href="/comments/${comment_data.id}">
+                  <i class="fas fa-trash-alt"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+      `
+
+    if (comment_data.product_seller.id == comment_data.user_id){
+      // 出品者とコメントしたユーザーが等しい場合
+      var html = HTML_user + HTML_seller + HTML_body
+    } else {
+      // 出品者とコメントしたユーザーが異なる場合
+      var html = HTML_user + HTML_body
+    };
+
+    return html; 
+  };
+
+  // ===================================
+  // 仮削除表示用
+  // ===================================
+
+  function preDelete(index){
+    var html = 
+    `
+    <div class="content-main__comments__list__block__content">
+      <div class="content-main__comments__list__block__content__text-deleted">
+      出品者によりこのコメントは削除されました
+      </div>
+      <div class="content-main__comments__list__block__content__icons">
+        <div class="content-main__comments__list__block__content__icons__right">
+          <div class="content-main__comments__list__block__content__icons__right__restore" data-index=${index}>
+            <a href="/comments/${index}/restore">
+              <i class="fas fa-trash-restore"></i>
+            </a>
+          </div>
+          <div class="content-main__comments__list__block__content__icons__right__delete complete__delete" data-index=${index}>
+            <a rel="nofollow" data-method="delete" href="/comments/${index}">
+              <i class="fas fa-trash-alt"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div> 
+    `
+    return html;
+  };
+  // ===================================
+  // コメント作成した場合
+  // ===================================
   $('.content-main__comments__form__new').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -85,4 +183,64 @@ $(document).on('turbolinks:load', function() {
       alert("コメントの送信に失敗しました");
     });
   });
+
+  // ===================================
+  // 復元した場合
+  // ===================================
+  $(".content-main__comments__list").on('click',".comment_restore",function(e){
+    e.preventDefault()
+    var index = $(this).data("index")
+    var url =`/comments/${index}/restore`
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: 'json',
+    })
+    .done(function(comment_data){
+      if (comment_data.item_seller.id == comment_data.user_id){   // 出品者とコメントユーザーが同じ場合
+        var html = new_comment(comment_data);
+        $(`.content-main__comments__list__block[data-index=${index}]`).replaceWith(html)
+      }else{    // 出品者とコメントユーザーが異なる場合
+        var html = restore_other_comment(comment_data);
+        $(`.content-main__comments__list__block[data-index=${index}]`).replaceWith(html)
+      }
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+
+  // ===================================
+  // 自分のコメントを仮削除した場合
+  // ===================================
+
+  $(".content-main__comments__list").on('click',".self_pre_delete",function(e){
+    e.preventDefault()
+    var index = $(this).data("index");
+    var content =  $(`.content-main__comments__list__block[data-index=${index}]`).find(".comment_content");
+    content.empty();
+    content.append(PLEdelete(index));
+  });
+
+  // ===================================
+  // 他人のコメントを仮削除した場合
+  // ===================================
+
+  $(".content-main__comments__list").on('click',".other_pre_delete",function(e){
+    e.preventDefault()
+    var index = $(this).data("index");
+    var content =  $(`.content-main__comments__list__block[data-index=${index}]`).find(".comment_content_other");
+    content.empty();
+    content.append(preDelete(index));
+  });
+
+  // ===================================
+  // 完全削除した場合
+  // ===================================
+  $(".content-main__comments__list").on('click','.complete_delete',function(e){
+    e.preventDefault()
+    var index = $(this).data("index");
+    $(`.content-main__comments__list__block[data-index=${index}]`).remove();
+  });
+
 });
