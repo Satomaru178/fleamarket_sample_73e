82258179d22c0ac273cc070639920ef72874c8de
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2020-08-22 20:19:40 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -74,7 +74,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @products = Product.where(category_id: @product.category_id).where.not(id: @product.id).order('created_at DESC').first(3)
+    @products = Product.includes(:images).where(category_id: @product.category_id).where.not(id: @product.id).order('created_at DESC').first(3)
     @comment = Comment.new
     @comments = @product.comments
   end
