1b897641de3d360b770d19defaab5f04312e3824
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2020-08-22 20:20:18 +0900

    fix registrations_controller.rb by bullet alert

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up
   def new
-    @user = User.new
+    @user = User.includes(:account).new
     @user.build_account
     @user.account.profile = ""
     @user.save
