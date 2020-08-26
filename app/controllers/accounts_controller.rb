b8f5408fe4393cfd6317dfb4b634039d49ca014b
Yuta1634 <yuta1994year@gmail.com>
2020-08-26 09:45:14 +0900

    Update accounts_controller.rb

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -52,7 +52,7 @@ class AccountsController < ApplicationController
   def show
     @profile = Account.find(params[:id])
     @user = @profile.user
-    @products = Product.where(seller_id: current_user.id)
+    @products = Product.where(seller_id: @user)
   end
 
 
